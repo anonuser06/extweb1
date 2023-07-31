a6802ca11e24f3a8606205c70d444741160dc2b2
anonuser06 <139946405+anonuser06@users.noreply.github.com>
2023-07-31 14:07:24 +0300

    Add files via upload

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'','password'=>'','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/omps/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+// if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"challenge1.tbtkext.gov.tr");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"4sJ5*9caTcjeGswp@8t6");
+if(!defined('DB_NAME')) define('DB_NAME',"ctf_db");
+?>
